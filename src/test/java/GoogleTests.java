3864ecb8a557c6af09fb3c2dd1663f20e08ea7e6
z000mg <zooomg2015@yandex.ru>
2020-12-02 16:19:42 +0300

    final

---
+++ src/test/java/GoogleTests.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleTests {
+
+    @BeforeAll
+    static void setup(){
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    public void willGoogleFindQAGuru () {
+        open("https://google.com");
+        $(byName("q")).val("QAGURU").pressEnter();
+        $("#res").shouldHave(text("QAGURU"));
+    }
+}
